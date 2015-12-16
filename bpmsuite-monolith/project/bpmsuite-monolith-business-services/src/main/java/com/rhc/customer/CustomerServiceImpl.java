96fa3816c0d2d81e949520de9f66bc4450f9ad5e
rahman <orisdev@gmail.com>
2015-12-16 11:47:26 -0500

    Completed steps 1-3

--- bpmsuite-monolith/project/bpmsuite-monolith-business-services/src/main/java/com/rhc/customer/CustomerServiceImpl.java
+++ bpmsuite-monolith/project/bpmsuite-monolith-business-services/src/main/java/com/rhc/customer/CustomerServiceImpl.java
@@ -12,6 +12,7 @@ import org.jbpm.services.api.UserTaskService;
 import org.jbpm.services.api.model.DeploymentUnit;
 import org.jbpm.services.api.model.ProcessInstanceDesc;
 
+import com.rhc.aggregates.Customer;
 import com.rhc.services.CustomerService;
 
 public class CustomerServiceImpl implements CustomerService {
@@ -29,13 +30,13 @@ public class CustomerServiceImpl implements CustomerService {
 	private UserTaskService userTaskService;
 
 	@Override
-	public Long startCustomerOnboardProcess(String firstName, String lastName) {
+	public Long startCustomerOnboardProcess(Customer customer) {
 
 		ensureCustomerKieJarIsDeploy();
 
 		Map<String, Object> processVariables = new HashMap<String, Object>();
-		processVariables.put("CustomerFirstName", firstName);
-		processVariables.put("CustomerLastName", lastName);
+		processVariables.put("CustomerFirstName", customer.getFirstName());
+		processVariables.put("CustomerLastName", customer.getLastName());
 
 		Long processId = processService.startProcess(DEPLOYMENT_UNIT.getIdentifier(), CUSTOMER_ONBOARD_PROCESS_ID, processVariables);
 		return processId;
--- bpmsuite-monolith/project/bpmsuite-monolith-business-services/src/test/java/com/rhc/customer/CustomerServiceTest.java
+++ bpmsuite-monolith/project/bpmsuite-monolith-business-services/src/test/java/com/rhc/customer/CustomerServiceTest.java
@@ -3,17 +3,21 @@ package com.rhc.customer;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.rhc.aggregates.Customer;
 import com.rhc.services.AbstractBusinessServicesTest;
 
 public class CustomerServiceTest extends AbstractBusinessServicesTest{
 	
 	@Test
 	public void shouldSuccessfullyStartACustomerOnboardProcess(){
+		Customer c= new Customer();
+		c.setFirstName("Joe");
+		c.setLastName("Wohar");
 		// given
 		Assert.assertNotNull(customerService);
 		
 		// when 
-		Long processId = customerService.startCustomerOnboardProcess("Leia", "Organa");
+		Long processId = customerService.startCustomerOnboardProcess(c);
 		
 		// then
 		Assert.assertEquals( new Long(1), processId);
